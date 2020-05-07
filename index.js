822e9bf94924f54108ec1d17a8db69d916d98c63
Maher Suleyman <maher.suleyman.ms@gmail.com>
2020-05-07 18:28:52 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+class BoardMember{
+    constructor(newName, newHomeState, newTraining) {
+        this.name = newName;
+        this.homeState = newHomeState;
+        this.training = newTraining;
+    }
+    veto() {
+        return `No, I must disagree`
+    }
+    approve(){
+        return `You can do that!`
+    }
+    doCharity(){
+        return `I like to help people.`
+    }
+    releasePressStatement(){
+        return `You will see great things from Scuber.`
+    }
+    sayHi() {
+        return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    }
+}
